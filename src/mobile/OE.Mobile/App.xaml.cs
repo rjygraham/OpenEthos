89d36fb34815a4f7be95d2435cd6dfd5af137d71
Ryan Graham <rjygraham@live.com>
2021-01-23 20:51:50 -0500

    Renamed projects, namespaces, etc from Open to OE for OpenEthos

---
+++ src/mobile/OE.Mobile/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using OE.Mobile.Services;
+using OE.Mobile.ViewModels;
+using OE.Mobile.Views;
+using Prism;
+using Prism.Ioc;
+using Refit;
+
+namespace OE.Mobile
+{
+	public partial class App
+	{
+		public App()
+			: this(null)
+		{
+		}
+
+		public App(IPlatformInitializer initializer)
+			: base(initializer)
+		{
+		}
+
+		protected override async void OnInitialized()
+		{
+			InitializeComponent();
+
+			await NavigationService.NavigateAsync(nameof(MainPage));
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+			containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+
+			containerRegistry.RegisterInstance<IProfileApiService>(RestService.For<IProfileApiService>("https://66d47af52655.ngrok.io"));
+		}
+	}
+}
