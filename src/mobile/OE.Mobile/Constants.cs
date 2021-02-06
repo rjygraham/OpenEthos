4f7534f0c259765f4b6ac073a6f62732a5b62fc5
Ryan Graham <rjygraham@live.com>
2021-02-06 16:22:48 -0500

    Add MSBuild task to replace secrets at build time (#9)
    * Updated project with secret replacing task
    
    * Moved ReplaceSecretsTask to separate assembly for reuse across projects.

--- src/mobile/OE.Mobile/Constants.cs
+++ src/mobile/OE.Mobile/Constants.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace OE.Mobile
 {
@@ -9,19 +10,19 @@ namespace OE.Mobile
 		public static class AuthenticationSettings
 		{
 			// Azure AD B2C Coordinates
-			public const string Tenant = "twitchoedev.onmicrosoft.com";
-			public const string AzureADB2CHostname = "twitchoedev.b2clogin.com";
-			public const string ClientId = "d709ef3a-4134-46a0-9d1d-d7c335e5b728";
-			public const string PolicySignUpSignIn = "B2C_1_signup_signin";
+			public const string Tenant = "$B2C_TENANTID$";
+			public const string AzureADB2CHostname = "$B2C_HOSTNAME$";
+			public const string ClientId = "$B2C_CLIENTID$";
+			public const string PolicySignUpSignIn = "$B2C_SUSI_POLICY$";
 
-			public static string[] Scopes = { "openid", "profile", "offline_access", "https://twitchoedev.onmicrosoft.com/api/profile" };
+			public static string[] Scopes = { "openid", "profile", "offline_access", $"https://{Tenant}/api/profile" };
 
 			public static string AuthorityBase = $"https://{AzureADB2CHostname}/tfp/{Tenant}/";
 			public static string AuthoritySignInSignUp = $"{AuthorityBase}{PolicySignUpSignIn}";
 
-			public static string AppId = "com.companyname.openethos";
+			public static string AppId = "$XAM_MOBILE_APPID$";
 
-			public static string AndroidRedirect = $"msauth://{AppId}/T60n5E0JVPDmhnQda%2B%2F%2BG0eJV6M%3D";
+			public static string AndroidRedirect = $"msauth://{AppId}/$XAM_ANDROID_SIGHASH$";
 			public static string IosRedirect = $"msauth.{AppId}://auth";
 		}
 	}
